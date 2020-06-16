a70e4dd3aa4a6dc7e6bd839633181fb38c89a808
lalitgarg12 <lalitgarg12@gmail.com>
2020-06-16 21:21:29 +0530

    Modified Commit

--- 01IntroToTensorflow/Week1 - A New Paradigm/Week1 - Exercise.py
+++ 01IntroToTensorflow/Week1 - A New Paradigm/Week1 - Exercise.py
@@ -8,7 +8,8 @@
 
 import tensorflow as tf
 import numpy as np
-from tensorflow import  keras
+from tensorflow import keras
+
 
 def house_model(y_new):
     xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
@@ -17,9 +18,10 @@ def house_model(y_new):
         keras.layers.Dense(units=1, input_shape=[1])
     ])
     model.compile(optimizer='sgd',
-                  loss = 'mean_squared_error')
+                  loss='mean_squared_error')
     model.fit(xs, ys, epochs=1000)
     return model.predict(y_new)[0]
+
 
 prediction = house_model([7.0])
 
