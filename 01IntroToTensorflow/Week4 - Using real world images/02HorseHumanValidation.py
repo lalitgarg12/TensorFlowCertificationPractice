756e7f9638fdd799e86b66fc1da802903d9f9643
lalitgarg12 <lalitgarg12@gmail.com>
2020-06-18 08:10:54 +0530

    Week4 updated

---
+++ 01IntroToTensorflow/Week4 - Using real world images/02HorseHumanValidation.py
@@ -0,0 +1,117 @@
+import os
+import zipfile
+
+local_zip = 'E:/TensorFlow Coursera/horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('E:/TensorFlow Coursera/horse-or-human')
+local_zip = 'E:/TensorFlow Coursera/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('E:/TensorFlow Coursera/validation-horse-or-human')
+zip_ref.close()
+
+# Directory with our training horse pictures
+train_horse_dir = os.path.join('E:/TensorFlow Coursera/horse-or-human/horses')
+
+# Directory with our training human pictures
+train_human_dir = os.path.join('E:/TensorFlow Coursera/horse-or-human/humans')
+
+# Directory with our training horse pictures
+validation_horse_dir = os.path.join('E:/TensorFlow Coursera/validation-horse-or-human/horses')
+
+# Directory with our training human pictures
+validation_human_dir = os.path.join('E:/TensorFlow Coursera/validation-horse-or-human/humans')
+
+train_horse_names = os.listdir(train_horse_dir)
+train_human_names = os.listdir(train_human_dir)
+
+validation_horse_names = os.listdir(validation_horse_dir)
+validation_human_names = os.listdir(validation_human_dir)
+
+import tensorflow as tf
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    #tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    #tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    #tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    #tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['accuracy'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        'E:/TensorFlow Coursera/horse-or-human/',  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow training images in batches of 128 using train_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+        'E:/TensorFlow Coursera/validation-horse-or-human/',  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=32,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+history = model.fit(
+      train_generator,
+      steps_per_epoch=8,
+      epochs=15,
+      verbose=1,
+      validation_data = validation_generator,
+      validation_steps=8)
+
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+
+    # predicting images
+    path = '/content/' + fn
+    img = image.load_img(path, target_size=(150, 150))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    print(classes[0])
+    if classes[0] > 0.5:
+        print(fn + " is a human")
+    else:
+        print(fn + " is a horse")
+
+
+
