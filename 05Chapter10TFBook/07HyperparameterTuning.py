bb16ce5f8ee00fbd49e45153575bebcd22382a49
lalitgarg12 <lalitgarg12@gmail.com>
2020-06-25 16:51:18 +0530

    HandsOn Book CHapter 10

---
+++ 05Chapter10TFBook/07HyperparameterTuning.py
@@ -0,0 +1,95 @@
+import sklearn
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import numpy as np
+import os
+
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "ann"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+# Ignore useless warnings (see SciPy issue #5998)
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+X_new = X_test[:3]
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[8]):
+    model = keras.models.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=input_shape))
+    for layer in range(n_hidden):
+        model.add(keras.layers.Dense(n_neurons, activation="relu"))
+    model.add(keras.layers.Dense(1))
+    optimizer = keras.optimizers.SGD(lr=learning_rate)
+    model.compile(loss="mse", optimizer=optimizer)
+    return model
+
+keras_reg = keras.wrappers.scikit_learn.KerasRegressor(build_model)
+
+keras_reg.fit(X_train, y_train, epochs=100,
+              validation_data=(X_valid, y_valid),
+              callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+
+mse_test = keras_reg.score(X_test, y_test)
+
+y_pred = keras_reg.predict(X_new)
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+from scipy.stats import reciprocal
+from sklearn.model_selection import RandomizedSearchCV
+
+param_distribs = {
+    "n_hidden": [0, 1, 2, 3],
+    "n_neurons": np.arange(1, 100),
+    "learning_rate": reciprocal(3e-4, 3e-2),
+}
+
+rnd_search_cv = RandomizedSearchCV(keras_reg, param_distribs, n_iter=10, cv=3, verbose=2)
+rnd_search_cv.fit(X_train, y_train, epochs=100,
+                  validation_data=(X_valid, y_valid),
+                  callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+
+rnd_search_cv.best_params_
+rnd_search_cv.best_score_
+rnd_search_cv.best_estimator_
+rnd_search_cv.score(X_test, y_test)
+model = rnd_search_cv.best_estimator_.model
+model
+model.evaluate(X_test, y_test)
---
+++ 05Chapter10TFBook/09ExerciseMnist.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+from tensorflow import  keras
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.mnist.load_data()
+print(X_train_full.shape)
+print(X_train_full.dtype)
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.
+plt.imshow(X_train[0], cmap="binary")
+plt.axis('off')
+plt.show()
+print(y_train)
+print(X_valid.shape)
+print(X_test.shape)
+n_rows = 4
+n_cols = 10
+plt.figure(figsize=(n_cols * 1.2, n_rows * 1.2))
+for row in range(n_rows):
+    for col in range(n_cols):
+        index = n_cols * row + col
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(X_train[index], cmap="binary", interpolation="nearest")
+        plt.axis('off')
+        plt.title(y_train[index], fontsize=12)
+plt.subplots_adjust(wspace=0.2, hspace=0.5)
+plt.show()
+K = keras.backend
+
+class ExponentialLearningRate(keras.callbacks.Callback):
+    def __init__(self, factor):
+        self.factor = factor
+        self.rates = []
+        self.losses = []
+    def on_batch_end(self, batch, logs):
+        self.rates.append(K.get_value(self.model.optimizer.lr))
+        self.losses.append(logs["loss"])
+        K.set_value(self.model.optimizer.lr, self.model.optimizer.lr * self.factor)
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.SGD(lr=1e-3),
+              metrics=["accuracy"])
+expon_lr = ExponentialLearningRate(factor=1.005)
+
+history = model.fit(X_train, y_train, epochs=1,
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[expon_lr])
+
+plt.plot(expon_lr.rates, expon_lr.losses)
+plt.gca().set_xscale('log')
+plt.hlines(min(expon_lr.losses), min(expon_lr.rates), max(expon_lr.rates))
+plt.axis([min(expon_lr.rates), max(expon_lr.rates), 0, expon_lr.losses[0]])
+plt.xlabel("Learning rate")
+plt.ylabel("Loss")
+plt.show()
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.SGD(lr=2e-1),
+              metrics=["accuracy"])
+
+run_index = 1 # increment this at every run
+run_logdir = os.path.join(os.curdir, "my_mnist_logs", "run_{:03d}".format(run_index))
+run_logdir
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=20)
+checkpoint_cb = keras.callbacks.ModelCheckpoint("my_mnist_model.h5", save_best_only=True)
+tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+
+history = model.fit(X_train, y_train, epochs=100,
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[early_stopping_cb, checkpoint_cb, tensorboard_cb])
+
+model = keras.models.load_model("my_mnist_model.h5") # rollback to best model
+model.evaluate(X_test, y_test)
+
