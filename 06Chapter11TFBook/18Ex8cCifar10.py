4fa55190def610a825d95772c5d694a222a4cb61
lalitgarg12 <lalitgarg12@gmail.com>
2020-07-05 07:47:36 +0530

    Chapter 11 Done

---
+++ 06Chapter11TFBook/18Ex8cCifar10.py
@@ -0,0 +1,92 @@
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# try:
+#     # %tensorflow_version only exists in Colab.
+#     %tensorflow_version 2.x
+# except Exception:
+#     pass
+
+# TensorFlow ≥2.0 is required
+import tensorflow as tf
+from tensorflow import keras
+assert tf.__version__ >= "2.0"
+
+#%load_ext tensorboard
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+#%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "deep"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def logit(z):
+    return 1 / (1 + np.exp(-z))
+
+z = np.linspace(-5, 5, 200)
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.cifar10.load_data()
+
+X_train = X_train_full[5000:]
+y_train = y_train_full[5000:]
+X_valid = X_train_full[:5000]
+y_valid = y_train_full[:5000]
+
+keras.backend.clear_session()
+tf.random.set_seed(42)
+np.random.seed(42)
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[32, 32, 3]))
+model.add(keras.layers.BatchNormalization())
+for _ in range(20):
+    model.add(keras.layers.Dense(100, kernel_initializer="he_normal"))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation("elu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+optimizer = keras.optimizers.Nadam(lr=5e-4)
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=optimizer,
+              metrics=["accuracy"])
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=20)
+model_checkpoint_cb = keras.callbacks.ModelCheckpoint("my_cifar10_bn_model.h5", save_best_only=True)
+run_index = 1 # increment every time you train the model
+run_logdir = os.path.join(os.curdir, "my_cifar10_bn_logs", "run_bn_{:03d}".format(run_index))
+tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+callbacks = [early_stopping_cb, model_checkpoint_cb, tensorboard_cb]
+
+model.fit(X_train, y_train, epochs=100,
+          validation_data=(X_valid, y_valid),
+          callbacks=callbacks)
+
+model = keras.models.load_model("my_cifar10_bn_model.h5")
+print(model.evaluate(X_valid, y_valid))
---
+++ 06Chapter11TFBook/19Ex8dCifar10.py
@@ -0,0 +1,97 @@
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# try:
+#     # %tensorflow_version only exists in Colab.
+#     %tensorflow_version 2.x
+# except Exception:
+#     pass
+
+# TensorFlow ≥2.0 is required
+import tensorflow as tf
+from tensorflow import keras
+assert tf.__version__ >= "2.0"
+
+#%load_ext tensorboard
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+#%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "deep"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def logit(z):
+    return 1 / (1 + np.exp(-z))
+
+z = np.linspace(-5, 5, 200)
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.cifar10.load_data()
+
+X_train = X_train_full[5000:]
+y_train = y_train_full[5000:]
+X_valid = X_train_full[:5000]
+y_valid = y_train_full[:5000]
+
+keras.backend.clear_session()
+tf.random.set_seed(42)
+np.random.seed(42)
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[32, 32, 3]))
+for _ in range(20):
+    model.add(keras.layers.Dense(100,
+                                 kernel_initializer="lecun_normal",
+                                 activation="selu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+optimizer = keras.optimizers.Nadam(lr=7e-4)
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=optimizer,
+              metrics=["accuracy"])
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=20)
+model_checkpoint_cb = keras.callbacks.ModelCheckpoint("my_cifar10_selu_model.h5", save_best_only=True)
+run_index = 1 # increment every time you train the model
+run_logdir = os.path.join(os.curdir, "my_cifar10__selu_logs", "run_selu_{:03d}".format(run_index))
+tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+callbacks = [early_stopping_cb, model_checkpoint_cb, tensorboard_cb]
+
+X_means = X_train.mean(axis=0)
+X_stds = X_train.std(axis=0)
+X_train_scaled = (X_train - X_means) / X_stds
+X_valid_scaled = (X_valid - X_means) / X_stds
+X_test_scaled = (X_test - X_means) / X_stds
+
+model.fit(X_train_scaled, y_train, epochs=100,
+          validation_data=(X_valid_scaled, y_valid),
+          callbacks=callbacks)
+
+model = keras.models.load_model("my_cifar10_selu_model.h5")
+print(model.evaluate(X_valid_scaled, y_valid))
