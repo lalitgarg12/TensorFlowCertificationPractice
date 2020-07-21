6727ff153094903cf8f30a6f540f16870af250b2
lalitgarg12 <lalitgarg12@gmail.com>
2020-07-21 12:27:27 +0530

    Chapter 15 In Progress

---
+++ 10Chapter15TFBook/02ForecastingBefore.py
@@ -0,0 +1,231 @@
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# try:
+#     # %tensorflow_version only exists in Colab.
+#     %tensorflow_version 2.x
+#     IS_COLAB = True
+# except Exception:
+#     IS_COLAB = False
+
+# TensorFlow ≥2.0 is required
+import tensorflow as tf
+from tensorflow import keras
+assert tf.__version__ >= "2.0"
+
+# if not tf.config.list_physical_devices('GPU'):
+#     print("No GPU was detected. LSTMs and CNNs can be very slow without a GPU.")
+#     if IS_COLAB:
+#         print("Go to Runtime > Change runtime and select a GPU hardware accelerator.")
+
+# Common imports
+import numpy as np
+import os
+from pathlib import Path
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# To plot pretty figures
+# %matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "rnn"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def generate_time_series(batch_size, n_steps):
+    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
+    time = np.linspace(0, 1, n_steps)
+    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
+    return series[..., np.newaxis].astype(np.float32)
+
+np.random.seed(42)
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 1)
+X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
+X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
+
+print(X_train.shape), print(y_train.shape)
+
+def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
+    plt.plot(series, ".-")
+    if y is not None:
+        plt.plot(n_steps, y, "bx", markersize=10)
+    if y_pred is not None:
+        plt.plot(n_steps, y_pred, "ro")
+    plt.grid(True)
+    if x_label:
+        plt.xlabel(x_label, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label, fontsize=16, rotation=0)
+    plt.hlines(0, 0, 100, linewidth=1)
+    plt.axis([0, n_steps + 1, -1, 1])
+
+fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(12, 4))
+for col in range(3):
+    plt.sca(axes[col])
+    plot_series(X_valid[col, :, 0], y_valid[col, 0],
+                y_label=("$x(t)$" if col==0 else None))
+save_fig("time_series_plot")
+plt.show()
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20),
+    keras.layers.Dense(1)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+np.random.seed(43) # not 42, as it would give the first series in the train set
+
+series = generate_time_series(1, n_steps + 10)
+X_new, Y_new = series[:, :n_steps], series[:, n_steps:]
+X = X_new
+for step_ahead in range(10):
+    y_pred_one = model.predict(X[:, step_ahead:])[:, np.newaxis, :]
+    X = np.concatenate([X, y_pred_one], axis=1)
+
+Y_pred = X[:, n_steps:]
+
+print(Y_pred.shape)
+
+def plot_multiple_forecasts(X, Y, Y_pred):
+    n_steps = X.shape[1]
+    ahead = Y.shape[1]
+    plot_series(X[0, :, 0])
+    plt.plot(np.arange(n_steps, n_steps + ahead), Y[0, :, 0], "ro-", label="Actual")
+    plt.plot(np.arange(n_steps, n_steps + ahead), Y_pred[0, :, 0], "bx-", label="Forecast", markersize=10)
+    plt.axis([0, n_steps + ahead, -1, 1])
+    plt.legend(fontsize=14)
+
+plot_multiple_forecasts(X_new, Y_new, Y_pred)
+save_fig("forecast_ahead_plot")
+plt.show()
+
+np.random.seed(42)
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 10)
+X_train, Y_train = series[:7000, :n_steps], series[:7000, -10:, 0]
+X_valid, Y_valid = series[7000:9000, :n_steps], series[7000:9000, -10:, 0]
+X_test, Y_test = series[9000:, :n_steps], series[9000:, -10:, 0]
+
+X = X_valid
+for step_ahead in range(10):
+    y_pred_one = model.predict(X)[:, np.newaxis, :]
+    X = np.concatenate([X, y_pred_one], axis=1)
+
+Y_pred = X[:, n_steps:, 0]
+
+print(Y_pred.shape)
+
+print(np.mean(keras.metrics.mean_squared_error(Y_valid, Y_pred)))
+
+Y_naive_pred = Y_valid[:, -1:]
+print(np.mean(keras.metrics.mean_squared_error(Y_valid, Y_naive_pred)))
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[50, 1]),
+    keras.layers.Dense(10)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, Y_train, epochs=20,
+                    validation_data=(X_valid, Y_valid))
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20),
+    keras.layers.Dense(10)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, Y_train, epochs=20,
+                    validation_data=(X_valid, Y_valid))
+
+np.random.seed(43)
+
+series = generate_time_series(1, 50 + 10)
+X_new, Y_new = series[:, :50, :], series[:, -10:, :]
+Y_pred = model.predict(X_new)[..., np.newaxis]
+
+plot_multiple_forecasts(X_new, Y_new, Y_pred)
+plt.show()
+
+np.random.seed(42)
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 10)
+X_train = series[:7000, :n_steps]
+X_valid = series[7000:9000, :n_steps]
+X_test = series[9000:, :n_steps]
+Y = np.empty((10000, n_steps, 10))
+for step_ahead in range(1, 10 + 1):
+    Y[..., step_ahead - 1] = series[..., step_ahead:step_ahead + n_steps, 0]
+Y_train = Y[:7000]
+Y_valid = Y[7000:9000]
+Y_test = Y[9000:]
+
+print(X_train.shape), print(Y_train.shape)
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20, return_sequences=True),
+    keras.layers.TimeDistributed(keras.layers.Dense(10))
+])
+
+def last_time_step_mse(Y_true, Y_pred):
+    return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
+
+model.compile(loss="mse", optimizer=keras.optimizers.Adam(lr=0.01), metrics=[last_time_step_mse])
+history = model.fit(X_train, Y_train, epochs=20,
+                    validation_data=(X_valid, Y_valid))
+
+np.random.seed(43)
+
+series = generate_time_series(1, 50 + 10)
+X_new, Y_new = series[:, :50, :], series[:, 50:, :]
+Y_pred = model.predict(X_new)[:, -1][..., np.newaxis]
+
+plot_multiple_forecasts(X_new, Y_new, Y_pred)
+plt.show()
+
+
+
+
