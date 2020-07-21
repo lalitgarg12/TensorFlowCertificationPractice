a64826d9e02cd760d5058d51f192190b5ec9d8fe
lalitgarg12 <lalitgarg12@gmail.com>
2020-07-21 11:42:59 +0530

    Chapter 15 Started

---
+++ 10Chapter15TFBook/01BasicRNN.py
@@ -0,0 +1,197 @@
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# try:
+#     # %tensorflow_version only exists in Colab.
+#     %tensorflow_version 2.x
+#     IS_COLAB = True
+# except Exception:
+#     IS_COLAB = False
+
+# TensorFlow ≥2.0 is required
+import tensorflow as tf
+from tensorflow import keras
+assert tf.__version__ >= "2.0"
+
+# if not tf.config.list_physical_devices('GPU'):
+#     print("No GPU was detected. LSTMs and CNNs can be very slow without a GPU.")
+#     if IS_COLAB:
+#         print("Go to Runtime > Change runtime and select a GPU hardware accelerator.")
+
+# Common imports
+import numpy as np
+import os
+from pathlib import Path
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# To plot pretty figures
+# %matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "rnn"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def generate_time_series(batch_size, n_steps):
+    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
+    time = np.linspace(0, 1, n_steps)
+    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
+    return series[..., np.newaxis].astype(np.float32)
+
+np.random.seed(42)
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 1)
+X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
+X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
+
+print(X_train.shape), print(y_train.shape)
+
+def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
+    plt.plot(series, ".-")
+    if y is not None:
+        plt.plot(n_steps, y, "bx", markersize=10)
+    if y_pred is not None:
+        plt.plot(n_steps, y_pred, "ro")
+    plt.grid(True)
+    if x_label:
+        plt.xlabel(x_label, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label, fontsize=16, rotation=0)
+    plt.hlines(0, 0, 100, linewidth=1)
+    plt.axis([0, n_steps + 1, -1, 1])
+
+fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(12, 4))
+for col in range(3):
+    plt.sca(axes[col])
+    plot_series(X_valid[col, :, 0], y_valid[col, 0],
+                y_label=("$x(t)$" if col==0 else None))
+save_fig("time_series_plot")
+plt.show()
+
+y_pred = X_valid[:, -1]
+np.mean(keras.losses.mean_squared_error(y_valid, y_pred))
+
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[50, 1]),
+    keras.layers.Dense(1)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+def plot_learning_curves(loss, val_loss):
+    plt.plot(np.arange(len(loss)) + 0.5, loss, "b.-", label="Training loss")
+    plt.plot(np.arange(len(val_loss)) + 1, val_loss, "r.-", label="Validation loss")
+    plt.gca().xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
+    plt.axis([1, 20, 0, 0.05])
+    plt.legend(fontsize=14)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.grid(True)
+
+plot_learning_curves(history.history["loss"], history.history["val_loss"])
+plt.show()
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(1, input_shape=[None, 1])
+])
+
+optimizer = keras.optimizers.Adam(lr=0.005)
+model.compile(loss="mse", optimizer=optimizer)
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+plot_learning_curves(history.history["loss"], history.history["val_loss"])
+plt.show()
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20, return_sequences=True),
+    keras.layers.SimpleRNN(1)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+plot_learning_curves(history.history["loss"], history.history["val_loss"])
+plt.show()
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20),
+    keras.layers.Dense(1)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+plot_learning_curves(history.history["loss"], history.history["val_loss"])
+plt.show()
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+plt.show()
+
